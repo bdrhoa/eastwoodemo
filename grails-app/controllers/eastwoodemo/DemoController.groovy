11f37c5dc0ee5bcd9a86a7e92f151313266153fe
Brad Rhoads <bdrhoa@gmail.com>
2012-07-04 11:45:07 -0600

    Added comment about having to use  &amp; instead of & in the links. Also added a new saveToFile.
    saveToFile should generate a .pdf to the file system and not send it to
    the client. I followed the docs, but only an empty .pdf is created.

--- grails-app/controllers/eastwoodemo/DemoController.groovy
+++ grails-app/controllers/eastwoodemo/DemoController.groovy
@@ -1,12 +1,21 @@
 package eastwoodemo
 
 class DemoController {
-	
+	def pdfRenderingService
 
 	def index  = {
 		
 			renderPdf(template: "charts", model: [stuff:'hello'], filename: "chart.pdf")
 			
 	}
+	
+	def saveToFile = {
+		new File("localchart.pdf").withOutputStream { outputStream ->
+	    	pdfRenderingService.render(controller:'demo', template: 'charts', model: [stuff:'hello'])
+		}
+		
+		render "saveToFile"
+	}
+	
 	
 }
--- grails-app/views/demo/index.gsp
+++ grails-app/views/demo/index.gsp
@@ -1,1 +1,2 @@
+// Note that you have to use &amp; instead of & in the links. See http://java.net/projects/xhtmlrenderer/lists/users/archive/2008-03/message/25
 <g:render template="charts" />
